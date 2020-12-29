01a9a645a64bcd4e5ea3a83f1bbc74f410eef839
HybridSystemsLab <ricardo@soe.ucsc.edu>
2020-12-28 17:07:39 -0800

    Add files via upload

---
+++ OscillatorSyncContinuous/initialization_Oscillator_Sync_Continuous.m
@@ -0,0 +1,39 @@
+%--------------------------------------------------------------------------
+% Project: Hybrid Feedback Control book
+% Description: Synchronization of Oscillators with
+% Continuous Measurements
+% https://hybrid.soe.ucsc.edu/software
+% http://hybridsimulator.wordpress.com/
+% Filename: initialization_Oscillator_Sync_Continuous
+% Set of simulation files created and edited by 
+% Yusheng Fang and Ricardo Sanfelice
+%--------------------------------------------------------------------------
+
+
+% clear all                                                               
+                                                                        
+% initial conditions                                                    
+x1_0 = [-1;0];                                                            
+x2_0 = [1;0];
+x3_0 = [0.5;0];
+x4_0 = [2;0];  
+
+%global constants
+global Kmatrix;
+% Amatrix = [0 1;-1 0];
+% Bmatrix = [0;1];
+Kmatrix = [-1 -2];
+
+% simulation horizon                                                    
+T = 15;                                                                 
+J = 15;                                                                 
+                                                                        
+% rule for jumps                                                        
+% rule = 1 -> priority for jumps                                        
+% rule = 2 -> priority for flows                                        
+% rule = 3 -> no priority, random selection when simultaneous conditions
+rule = 2;                                                               
+                                                                        
+%solver tolerances
+RelTol = 1e-8;
+MaxStep = .005;
---
+++ OscillatorSyncContinuous/postprocessing_Oscillator_Sync_Continuous.m
@@ -0,0 +1,33 @@
+%--------------------------------------------------------------------------
+% Project: Hybrid Feedback Control book
+% Description: Synchronization of Oscillators with
+% Continuous Measurements
+% https://hybrid.soe.ucsc.edu/software
+% http://hybridsimulator.wordpress.com/
+% Filename: postprocessing_Oscillator_Sync_Continuous
+% Set of simulation files created and edited by 
+% Yusheng Fang and Ricardo Sanfelice
+%--------------------------------------------------------------------------
+
+close all
+figure(1)
+plot(x1(:,1),x1(:,2),'b-')
+grid on
+hold on
+plot(x2(:,1),x2(:,2),'g-')
+hold on
+plot(x3(:,1),x3(:,2),'y-')
+hold on
+plot(x4(:,1),x4(:,2),'r-')
+legend('z_{1}', 'z_{2}', 'z_{3}','z_{4}')
+xlabel ('z_{i},1')
+ylabel ('z_{i},2')
+hold off
+figure(2)
+error = zeros(length(t1), 1);
+for i = 1:length(t1)
+error(i) = norm(x1(i, :) - x2(i, :) + x1(i, :) - x3(i, :) + x1(i, :) - x4(i, :));
+end
+plot(t1, error)
+grid on
+hold on
